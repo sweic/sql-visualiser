08a89edf7ea0606bf873bc025eefa0e4e7988eff
sweic <cswei019@gmail.com>
2023-05-10 12:47:19 +0800

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyles from "./styles/globalStyles";
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyles />
+    <Analytics />
     <App />
   </React.StrictMode>
 );
